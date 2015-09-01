78e6276d8c017158eafbc56884594d4ba4945217
Giorgio Pellero <giorgio@pellero.it>
2015-09-01 20:33:39 +0200

    Fix logger regex crash

--- src/maild/logger.cr
+++ src/maild/logger.cr
@@ -10,10 +10,10 @@ end
 
 class Object
   macro info(message)
-    puts "#{TimeFormat.new(LOG_TIME_FORMAT).format Time.now} - INFO  - #{caller[0][/::(\w+)#(\w+)</i, 1]}##{caller[0][/::(\w+)#(\w+)</i, 2]}: #{{{message}}}"
+    puts "#{TimeFormat.new(LOG_TIME_FORMAT).format Time.now} - INFO  - #{caller[0][/::(\w+)#(\w+)</i, 1]?}##{caller[0][/::(\w+)#(\w+)</i, 2]?}: #{{{message}}}"
   end
 
   macro error(message)
-    puts "#{TimeFormat.new(LOG_TIME_FORMAT).format Time.now} - ERROR - #{caller[0][/::(\w+)#(\w+)</i, 1]}##{caller[0][/::(\w+)#(\w+)</i, 2]}:#{__LINE__}: #{{{message}}}"
+    puts "#{TimeFormat.new(LOG_TIME_FORMAT).format Time.now} - ERROR - #{caller[0][/::(\w+)#(\w+)</i, 1]?}##{caller[0][/::(\w+)#(\w+)</i, 2]?}:#{__LINE__}: #{{{message}}}"
   end
 end
