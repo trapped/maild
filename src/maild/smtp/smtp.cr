fa06fb0fcd7146b1f46cd2481d1f4eda9c658901
Giorgio Pellero <giorgio@pellero.it>
2015-09-03 09:07:36 +0200

    Better QUIT, partial EHLO, argument_missing

--- src/maild/smtp/smtp.cr
+++ src/maild/smtp/smtp.cr
@@ -1,4 +1,6 @@
 class Maild::SMTP < Maild::Handler
+  property identity
+
   def parse(cmd : String)
     return cmd.chomp.split ' '
   end
@@ -12,6 +14,7 @@ class Maild::SMTP < Maild::Handler
         cmd = parse line
         info "#{cmd[0].upcase} from #{sock.peeraddr.ip_address}:#{sock.peeraddr.ip_port}"
         handle sock, cmd.shift.upcase, cmd
+        break if sock.closed?
       end
     rescue IO::Timeout
       info "Connection timed out"
@@ -19,13 +22,16 @@ class Maild::SMTP < Maild::Handler
     rescue ex
       info "Connection terminated unexpectedly: #{ex.inspect}"
     end
-    sock.close
     info "Closed"
   end
 
-  def method_missing(sock, cmd)
+  def self.method_missing(sock, cmd)
     sock.puts "504 not implemented"
     error "#{cmd.upcase.inspect} requested but not implemented (available: #{@@handlers.try &.keys.join ", "})"
+  end
+
+  def self.argument_missing(sock, arg)
+    sock.puts "501 argument missing: #{arg}"
   end
 
   private def greet(sock)
@@ -39,5 +45,16 @@ class Maild::SMTP < Maild::Handler
   handle "quit" do |sock|
     sock.puts "221 maild ESMTP server closing connection"
     sock.close
+  end
+
+  handle "ehlo" do |sock, args|
+    @identity = requires args[0] as String
+    capabilities = [
+      "greetings, #{identity}!",
+      "8BITMIME"
+    ]
+    capabilities.each_with_index do |capability, i|
+      sock.puts "250#{i == capabilities.length-1 ? '-' : ' '}#{capability}"
+    end
   end
 end
