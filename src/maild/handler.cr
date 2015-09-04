72f299532653133825555b1b0fa49907d72b8018
Giorgio Pellero <giorgio@pellero.it>
2015-09-04 20:24:35 +0200

    Better Handler macros, must_have, must_not_have, required

--- src/maild/handler.cr
+++ src/maild/handler.cr
@@ -1,53 +1,32 @@
 require "socket"
 
 abstract class Maild::Handler
-  @@handlers = nil
-
-  def initialize
-    @@handlers = Hash(String, (TCPSocket, Array(String) -> (Nil))).new unless @@handlers
-  end
-
   abstract def method_missing(sock : TCPSocket, name : String)
   abstract def argument_missing(sock : TCPSocket, name : String)
   abstract def handle(sock : TCPSocket)
 
-  private def handle(sock : TCPSocket, cmd : String, line : Array(String))
-    if @@handlers.not_nil!.has_key? cmd.upcase
-      handler = @@handlers.not_nil!.fetch(cmd.upcase)
-      handler.call sock, line
-    else
-      method_missing sock, cmd.upcase
+  macro inherited
+    macro handle(sock, cmd, args)
+      case \{\{cmd.id}}.upcase
+      \{% for method in @type.methods.map(&.name.stringify).select(&.starts_with? "cmd_").map(&.gsub(/cmd_/, "")) %}
+      when \{\{method.upcase}}
+        cmd_\{\{method.id}}(sock, args)
+      \{% end %}
+      else
+        method_missing sock, \{\{cmd.id}}.upcase
+      end
     end
   end
 
-  def self.add_method(cmd : String, proc : (TCPSocket, Array(String) -> (Nil)))
-    if @@handlers.nil?
-      info "Reinstantiating @@handlers"
-      @@handlers = Hash(String, (TCPSocket, Array(String) -> (Nil))).new
-    end
-    @@handlers.not_nil![cmd.upcase] = proc
+  macro required(name, source)
+    {{name.id}} = {{source.id}} || return argument_missing sock, {{name}}
   end
 
-  macro handle(cmd)
-    def self.cmd_{{cmd.id}}(sock : TCPSocket, args : Array(String))
-      {{yield}}
-      return nil
-    end
-    self.add_method({{cmd}}, ->cmd_{{cmd.id}}(TCPSocket, Array(String)))
-    info "New handler: #{{{cmd}}}"
+  macro must_not_have(var)
+    return sock.puts "503 wrong session state" if {{var}}
   end
 
-  macro requires(cast)
-    #the 'as' keyword is reserved for cast exprs, but we can carve the values out
-    #https://github.com/manastech/crystal/blob/ff30e2eb976286e260ef33b3ba6340422a479fa9/src/compiler/crystal/macros/methods.cr#L927-L938
-    %value = nil
-    begin
-      %value = {{cast.obj.id}}
-    rescue IndexError
-      self.argument_missing sock, {{cast.to.stringify.downcase}}
-      return nil
-    end
-    %value = {{cast.obj.id}} as {{cast.to.id}}
-    %value
+  macro must_have(var)
+    return sock.puts "503 wrong session state" unless {{var}}
   end
 end
