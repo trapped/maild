2187b994a0286b1fc277b399dbc32975bfbcfc39
Giorgio Pellero <giorgio@pellero.it>
2015-09-04 20:27:26 +0200

    RSET, EHLO, HELO, MAIL, QUIT

--- src/maild/smtp/smtp.cr
+++ src/maild/smtp/smtp.cr
@@ -37,23 +37,51 @@ class Maild::SMTP < Maild::Handler
     sock.puts "220 maild ESMTP server ready"
   end
 
-  handle "noop" do |sock|
+  def cmd_noop(sock, args)
     sock.puts "250 no operation performed"
   end
 
-  handle "quit" do |sock|
+  def cmd_rset(sock, args)
+    {% for var in @type.instance_vars %}
+    {{var.id}} = nil
+    {% end %}
+    sock.puts "250 session has been reset"
+  end
+
+  def cmd_quit(sock, args)
     sock.puts "221 maild ESMTP server closing connection"
     sock.close
   end
 
-  handle "ehlo" do |sock, args|
-    @identity = requires args[0] as String
+  def cmd_ehlo(sock, args)
+    must_not_have @identity
+    @identity = required "identity", args[0]?
     capabilities = [
       "greetings, #{identity}!",
-      "8BITMIME"
+      "8BITMIME", # strings are UTF8 by default
+      "PIPELINING" # receive buffer is not flushed on failure
     ]
     capabilities.each_with_index do |capability, i|
-      sock.puts "250#{i == capabilities.length-1 ? '-' : ' '}#{capability}"
+      sock.puts "250#{i == capabilities.length-1 ? ' ' : '-'}#{capability}"
     end
+  end
+
+  def cmd_helo(sock, args)
+    must_not_have @identity
+    @identity = required "identity", args[0]?
+    sock.puts "250 greetings, #{identity}!"
+  end
+
+  def cmd_mail(sock, args)
+    must_have @identity
+    must_not_have @from
+    while arg = args.shift?
+      case arg.downcase
+      when "from:"
+        from = args.shift? || return argument_missing sock, "sender address"
+        @from = from if from =~ /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i || return sock.puts "553 invalid sender address"
+      end
+    end
+    sock.puts "250 sender #{@from} ok"
   end
 end
