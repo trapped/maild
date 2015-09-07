ec78a4273c7002f82b66a58214015896056ebf19
Giorgio Pellero <giorgio@pellero.it>
2015-09-07 20:20:41 +0200

    No logging if

--- src/maild/logger.cr
+++ src/maild/logger.cr
@@ -1,19 +1,29 @@
 LOG_TIME_FORMAT = "%d/%b/%Y:%H:%M:%S %z"
 
+$no_logging = false
+
 macro info(message)
-  puts "#{TimeFormat.new(LOG_TIME_FORMAT).format Time.now} - INFO  - #{__FILE__}: #{{{message}}}"
+  unless $no_logging
+    puts "#{TimeFormat.new(LOG_TIME_FORMAT).format Time.now} - INFO  - #{__FILE__}: #{{{message}}}"
+  end
 end
 
 macro error(message)
-  puts "#{TimeFormat.new(LOG_TIME_FORMAT).format Time.now} - ERROR - #{__FILE__}:#{__LINE__}: #{{{message}}}"
+  unless $no_logging
+    puts "#{TimeFormat.new(LOG_TIME_FORMAT).format Time.now} - ERROR - #{__FILE__}:#{__LINE__}: #{{{message}}}"
+  end
 end
 
 class Object
   macro info(message)
-    puts "#{TimeFormat.new(LOG_TIME_FORMAT).format Time.now} - INFO  - #{caller[0][/::(\w+)#(\w+)</i, 1]?}##{caller[0][/::(\w+)#(\w+)</i, 2]?}: #{{{message}}}"
+    unless $no_logging
+      puts "#{TimeFormat.new(LOG_TIME_FORMAT).format Time.now} - INFO  - #{caller[0][/::(\w+)#(\w+)</i, 1]?}##{caller[0][/::(\w+)#(\w+)</i, 2]?}: #{{{message}}}"
+    end
   end
 
   macro error(message)
-    puts "#{TimeFormat.new(LOG_TIME_FORMAT).format Time.now} - ERROR - #{caller[0][/::(\w+)#(\w+)</i, 1]?}##{caller[0][/::(\w+)#(\w+)</i, 2]?}:#{__LINE__}: #{{{message}}}"
+    unless $no_logging
+      puts "#{TimeFormat.new(LOG_TIME_FORMAT).format Time.now} - ERROR - #{caller[0][/::(\w+)#(\w+)</i, 1]?}##{caller[0][/::(\w+)#(\w+)</i, 2]?}:#{__LINE__}: #{{{message}}}"
+    end
   end
 end
