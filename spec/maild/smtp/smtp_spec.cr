fc7f92d733af74fc22c0d53194a698b43e8c8d27
Giorgio Pellero <giorgio@pellero.it>
2015-09-07 20:21:12 +0200

    Working SMTP spec

--- spec/maild/smtp/smtp_spec.cr
+++ spec/maild/smtp/smtp_spec.cr
@@ -4,35 +4,43 @@ require "../../../src/maild/io"
 require "../../../src/maild/smtp"
 require "socket"
 
+$no_logging = true
+
 class Maild::SMTP < Maild::Handler
   describe "#handle" do
     it "greets on connect" do
       smtp = Maild::SMTP.new
       ss = UNIXSocket.pair
       smtp.timeout = 5.seconds
+      spawn do
+        ss[0].gets.not_nil!.chomp.should eq "220 maild ESMTP server ready"
+        ss[0].puts "QUIT"
+      end
       smtp.handle(ss[1])
-      ss[0].gets.not_nil!.chomp.should eq "220 maild ESMTP server ready"
-      ss[0].close
     end
     it "accepts commands" do
       smtp = Maild::SMTP.new
       ss = UNIXSocket.pair
       smtp.timeout = 5.seconds
+      spawn do
+        ss[0].gets.not_nil!.chomp.should eq "220 maild ESMTP server ready"
+        ss[0].puts "NOOP"
+        ss[0].gets.not_nil!.chomp.should eq "250 no operation performed"
+        ss[0].puts "QUIT"
+      end
       smtp.handle(ss[1])
-      ss[0].gets.not_nil!.chomp.should eq "220 maild ESMTP server ready"
-      ss[0].puts "NOOP"
-      ss[0].gets.not_nil!.chomp.should eq "250 no operation performed"
-      ss[0].close
     end
     it "times out after a couple seconds" do
       smtp = Maild::SMTP.new
       ss = UNIXSocket.pair
       smtp.timeout = 2.seconds
+      spawn do
+        ss[0].gets.not_nil!.chomp.should eq "220 maild ESMTP server ready"
+        sleep 3.seconds
+        ss[0].gets.not_nil!.chomp.should eq "421 timed out"
+        ss[1].closed?.should be_true
+      end
       smtp.handle(ss[1])
-      ss[0].gets
-      sleep 3.seconds
-      ss[0].gets.not_nil!.chomp.should eq "421 timed out"
-      ss[1].closed?.should be_true
     end
   end
 end
