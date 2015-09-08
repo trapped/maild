7f5e51a1fc4fd3e115e482843a4f3d161abd1216
Giorgio Pellero <giorgio@pellero.it>
2015-09-08 14:05:43 +0200

    EHLO, HELO and MAIL tests

--- spec/maild/smtp/smtp_spec.cr
+++ spec/maild/smtp/smtp_spec.cr
@@ -125,5 +125,177 @@ class Maild::SMTP < Maild::Handler
       end
       smtp.handle(ss[1])
     end
+    it "sets identity" do
+      smtp = Maild::SMTP.new
+      ss = UNIXSocket.pair
+      smtp.timeout = 2.seconds
+      spawn do
+        ss[0].gets.not_nil!.chomp.should eq "220 maild ESMTP server ready"
+        ss[0].puts "EHLO example.com"
+        ss[0].gets.not_nil!.chomp.should eq "250-greetings, example.com!"
+        ss[0].puts "QUIT"
+      end
+      smtp.handle(ss[1])
+      smtp.identity.should eq "example.com"
+    end
+    it "shows capabilities" do
+      smtp = Maild::SMTP.new
+      ss = UNIXSocket.pair
+      smtp.timeout = 2.seconds
+      spawn do
+        ss[0].gets.not_nil!.chomp.should eq "220 maild ESMTP server ready"
+        ss[0].puts "EHLO example.com"
+        ss[0].gets.not_nil!.chomp.should eq "250-greetings, example.com!"
+        ss[0].gets.not_nil!.chomp.should eq "250-8BITMIME"
+        ss[0].gets.not_nil!.chomp.should eq "250 PIPELINING"
+        ss[0].puts "QUIT"
+      end
+      smtp.handle(ss[1])
+    end
+    it "doesn't allow being called twice" do
+      smtp = Maild::SMTP.new
+      ss = UNIXSocket.pair
+      smtp.timeout = 2.seconds
+      spawn do
+        ss[0].gets.not_nil!.chomp.should eq "220 maild ESMTP server ready"
+        ss[0].puts "EHLO example.com"
+        ss[0].gets.not_nil!.chomp.should eq "250-greetings, example.com!"
+        ss[0].gets
+        ss[0].gets
+        ss[0].puts "EHLO example.com"
+        ss[0].gets.not_nil!.chomp.should eq "503 wrong session state"
+      end
+      smtp.handle(ss[1])
+    end
+  end
+
+  describe "#cmd_helo" do
+    it "greets the client" do
+      smtp = Maild::SMTP.new
+      ss = UNIXSocket.pair
+      smtp.timeout = 2.seconds
+      spawn do
+        ss[0].gets.not_nil!.chomp.should eq "220 maild ESMTP server ready"
+        ss[0].puts "HELO example.com"
+        ss[0].gets.not_nil!.chomp.should eq "250 greetings, example.com!"
+        ss[0].puts "QUIT"
+      end
+      smtp.handle(ss[1])
+    end
+    it "sets identity" do
+      smtp = Maild::SMTP.new
+      ss = UNIXSocket.pair
+      smtp.timeout = 2.seconds
+      spawn do
+        ss[0].gets.not_nil!.chomp.should eq "220 maild ESMTP server ready"
+        ss[0].puts "HELO example.com"
+        ss[0].gets.not_nil!.chomp.should eq "250 greetings, example.com!"
+        ss[0].puts "QUIT"
+      end
+      smtp.handle(ss[1])
+      smtp.identity.should eq "example.com"
+    end
+    it "doesn't allow being called twice" do
+      smtp = Maild::SMTP.new
+      ss = UNIXSocket.pair
+      smtp.timeout = 2.seconds
+      spawn do
+        ss[0].gets.not_nil!.chomp.should eq "220 maild ESMTP server ready"
+        ss[0].puts "HELO example.com"
+        ss[0].gets.not_nil!.chomp.should eq "250 greetings, example.com!"
+        ss[0].puts "HELO example.com"
+        ss[0].gets.not_nil!.chomp.should eq "503 wrong session state"
+        ss[0].puts "QUIT"
+      end
+      smtp.handle(ss[1])
+    end
+  end
+
+  describe "#cmd_mail" do
+    it "requires being identified" do
+      smtp = Maild::SMTP.new
+      ss = UNIXSocket.pair
+      smtp.timeout = 2.seconds
+      spawn do
+        ss[0].gets.not_nil!.chomp.should eq "220 maild ESMTP server ready"
+        ss[0].puts "MAIL FROM:<user@example.com>"
+        ss[0].gets.not_nil!.chomp.should eq "503 wrong session state"
+        ss[0].puts "HELO example.com"
+        ss[0].gets
+        ss[0].puts "MAIL FROM:<user@example.com>"
+        ss[0].gets.not_nil!.chomp.should eq "250 sender user@example.com ok"
+        ss[0].puts "QUIT"
+      end
+      smtp.handle(ss[1])
+    end
+    it "doesn't allow being called twice" do
+      smtp = Maild::SMTP.new
+      ss = UNIXSocket.pair
+      smtp.timeout = 2.seconds
+      spawn do
+        ss[0].gets.not_nil!.chomp.should eq "220 maild ESMTP server ready"
+        ss[0].puts "HELO example.com"
+        ss[0].gets
+        ss[0].puts "MAIL FROM:<user@example.com>"
+        ss[0].gets.not_nil!.chomp.should eq "250 sender user@example.com ok"
+        ss[0].puts "MAIL FROM:<user@example.com>"
+        ss[0].gets.not_nil!.chomp.should eq "503 wrong session state"
+        ss[0].puts "QUIT"
+      end
+      smtp.handle(ss[1])
+    end
+    it "requires the sender address as argument" do
+      smtp = Maild::SMTP.new
+      ss = UNIXSocket.pair
+      smtp.timeout = 2.seconds
+      spawn do
+        ss[0].gets.not_nil!.chomp.should eq "220 maild ESMTP server ready"
+        ss[0].puts "HELO example.com"
+        ss[0].gets
+        ss[0].puts "MAIL"
+        ss[0].gets.not_nil!.chomp.should eq "501 argument missing: sender address"
+        ss[0].puts "MAIL FROM:<user@example.com>"
+        ss[0].gets.not_nil!.chomp.should eq "250 sender user@example.com ok"
+        ss[0].puts "QUIT"
+      end
+      smtp.handle(ss[1])
+    end
+    it "only accepts valid email addresses" do
+      smtp = Maild::SMTP.new
+      ss = UNIXSocket.pair
+      smtp.timeout = 2.seconds
+      spawn do
+        ss[0].gets.not_nil!.chomp.should eq "220 maild ESMTP server ready"
+        ss[0].puts "HELO example.com"
+        ss[0].gets
+        ss[0].puts "MAIL FROM:<user-example.com>"
+        ss[0].gets.not_nil!.chomp.should eq "553 invalid sender address"
+        ss[0].puts "MAIL FROM:user@example.com"
+        ss[0].gets.not_nil!.chomp.should eq "501 malformatted address"
+        ss[0].puts "MAIL FROM:<>"
+        ss[0].gets.not_nil!.chomp.should eq "553 invalid sender address"
+        ss[0].puts "MAIL FROM:"
+        ss[0].gets.not_nil!.chomp.should eq "501 argument missing: sender address"
+        ss[0].puts "MAIL FROM:<user@example.com>"
+        ss[0].gets.not_nil!.chomp.should eq "250 sender user@example.com ok"
+        ss[0].puts "QUIT"
+      end
+      smtp.handle(ss[1])
+    end
+    it "sets sender" do
+      smtp = Maild::SMTP.new
+      ss = UNIXSocket.pair
+      smtp.timeout = 2.seconds
+      spawn do
+        ss[0].gets.not_nil!.chomp.should eq "220 maild ESMTP server ready"
+        ss[0].puts "HELO example.com"
+        ss[0].gets
+        ss[0].puts "MAIL FROM:<user@example.com>"
+        ss[0].gets.not_nil!.chomp.should eq "250 sender user@example.com ok"
+        ss[0].puts "QUIT"
+      end
+      smtp.handle(ss[1])
+      smtp.sender.should eq "user@example.com"
+    end
   end
 end
