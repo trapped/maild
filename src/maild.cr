2d071f50f6ba06eb2610214933e0a7ace3325181
Giorgio Pellero <giorgio.pellero@gmail.com>
2015-10-09 14:26:46 +0200

    Print user count at startup

--- src/maild.cr
+++ src/maild.cr
@@ -3,5 +3,6 @@ require "./maild/smtp/*"
 require "./maild/pop3/*"
 
 info "Starting maild"
+info "#{Maild::User.all.size} users in database"
 list = Maild::Listener.new(2525, Maild::SMTP)
 list.start
